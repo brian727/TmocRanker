240ddd55cd58d5ac9a73348a6718f9c1927c5800
brian727 <brian.w.727@gmail.com>
2022-07-13 21:49:14 -0700

    delete, edit endpoints complete

--- src/main/java/com/briansdigital/p1TmocRunRanker/student/StudentController.java
+++ src/main/java/com/briansdigital/p1TmocRunRanker/student/StudentController.java
@@ -3,6 +3,7 @@ package com.briansdigital.p1TmocRunRanker.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -24,4 +25,17 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path="{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+        }
+
+        @PutMapping(path="{studentId}")
+        public void updateStudent(
+                    @PathVariable("studentId") Long studentId,
+                    @RequestParam(required = false) String name,
+                    @RequestParam(required = false) String email) {
+            studentService.updateStudent(studentId, name, email);
+        }
 }
--- src/main/java/com/briansdigital/p1TmocRunRanker/student/StudentService.java
+++ src/main/java/com/briansdigital/p1TmocRunRanker/student/StudentService.java
@@ -3,7 +3,9 @@ package com.briansdigital.p1TmocRunRanker.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -28,5 +30,30 @@ public class StudentService {
             throw new IllegalStateException("email taken");
         }
         studentRepo.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepo.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exist!");
+        }
+        studentRepo.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepo.findById(studentId).orElseThrow(() -> new IllegalStateException(
+                "student with id " + studentId + " does not exist!"
+        ));
+                if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+                    student.setName(name);
+                }
+                if(email !=null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+                    Optional<Student> studentOptional = studentRepo.findStudentByEmail(email);
+                    if (studentOptional.isPresent()) {
+                        throw new IllegalStateException("email taken!");
+                    }
+                    student.setEmail(email);
+                }
     }
 }
